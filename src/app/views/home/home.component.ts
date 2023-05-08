04d19fcd1c107d935d5e5ed127bd02c8dfcca3d8
Marlon <103782424+MarlonPasse@users.noreply.github.com>
2023-05-08 18:25:06 -0300

    melhorias na home

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,20 +9,35 @@ import { LoginService } from 'src/app/services/login.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+//instanciar a class User
 export class HomeComponent {
-
-  //instanciar a class User
-
-constructor (private loginService : LoginService){}
-  
-usermodel = new User()
+  constructor(private loginService: LoginService, private router: Router) { }
+  mensagemok = "Deu certo!"
+  mensagemerro = " "
+  dicas = ""
+  usermodel = new User()
 
   OnSubmit() {
     console.log(this.usermodel)
-    
-    this.loginService.login(this.usermodel).subscribe( (Response) => {
-      console.log(Response)
-    })
+    console.log(this.loginService.login)
+
+    this.loginService.login(this.usermodel).subscribe((Response) => {
+      //Se ok:
+      console.log(this.mensagemok)
+      this.router.navigateByUrl("/contato")
+    },
+      //Se erro:
+      (respErro) => {
+        //console.log(this.mensagemerro)
+        this.mensagemerro = respErro.error
+        if (this.mensagemerro == 'password is too short') {
+          this.mensagemerro = "A senha esta muito pequena!"
+          this.dicas = "Utilize mais de três caracteres"
+        }else{
+          this.dicas = ""
+        }
+      })
   }
 }
 
